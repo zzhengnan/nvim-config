28b05525d7ddc4a0950404ba8f48204233dffb3f
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2025-05-16 18:07:40 -0400

    Break up into plugin modules

---
+++ lua/plugins/appearance.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"Mofiqul/vscode.nvim",
+		priority = 100,
+		config = function()
+			vim.cmd.colorscheme("vscode")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = { theme = "vscode" },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "branch", "diff", "diagnostics" },
+				lualine_y = { "location" },
+				lualine_z = {},
+			},
+		},
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+}
