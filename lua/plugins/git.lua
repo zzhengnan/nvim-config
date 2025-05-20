f97a6b824a7a0863f3ab91b00bb836693cb69bbe
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2025-05-20 08:20:18 -0400

    Try neogit instead of vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,18 @@
 return {
-	"tpope/vim-fugitive",
+	-- "tpope/vim-fugitive",
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+			"echasnovski/mini.pick", -- optional
+			"folke/snacks.nvim", -- optional
+		},
+	},
 
 	{
 		"linrongbin16/gitlinker.nvim",
