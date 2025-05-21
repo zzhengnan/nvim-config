3d530610b0e89556945620ec151ec1cdd3e10db8
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2025-05-20 22:56:10 -0400

    Go back to vim-fugitive

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,18 +1,5 @@
 return {
-	-- "tpope/vim-fugitive",
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-
-			-- Only one of these is needed.
-			"nvim-telescope/telescope.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-			"echasnovski/mini.pick", -- optional
-			"folke/snacks.nvim", -- optional
-		},
-	},
+	"tpope/vim-fugitive",
 
 	{
 		"linrongbin16/gitlinker.nvim",
