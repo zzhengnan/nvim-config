7ca38d730c43681a11ece634e917774b0c6c3614
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2025-07-25 08:08:55 -0400

    Search for hidden files with ripgrep

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -45,11 +45,53 @@ return {
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
 				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
+				-- 	mappings = {
+				-- 	  i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+				-- 	},
+				--             }
+
+				-- https://stackoverflow.com/a/76991432
+				defaults = {
+					-- configure to use ripgrep
+					vimgrep_arguments = {
+						"rg",
+						"--follow", -- Follow symbolic links
+						"--hidden", -- Search for hidden files
+						"--no-heading", -- Don't group matches by each file
+						"--with-filename", -- Print the file path with the matched lines
+						"--line-number", -- Show line numbers
+						"--column", -- Show column numbers
+						"--smart-case", -- Smart case search
+
+						-- Exclude some patterns from search
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/yarn.lock",
+						"--glob=!**/package-lock.json",
+					},
+				},
+				pickers = {
+					find_files = {
+						hidden = true,
+						-- needed to exclude some files & dirs from general search
+						-- when not included or specified in .gitignore
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--glob=!**/.git/*",
+							"--glob=!**/.idea/*",
+							"--glob=!**/.vscode/*",
+							"--glob=!**/build/*",
+							"--glob=!**/dist/*",
+							"--glob=!**/yarn.lock",
+							"--glob=!**/package-lock.json",
+						},
+					},
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
