4530bc0cd0c1035c4d180716ee22ca81f4c8b6f0
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2026-01-29 22:43:08 -0500

    Add tokyonight

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,8 +2,17 @@ return {
 	{
 		"Mofiqul/vscode.nvim",
 		priority = 100,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("vscode")
+		-- end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
 		config = function()
-			vim.cmd.colorscheme("vscode")
+			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
 }
