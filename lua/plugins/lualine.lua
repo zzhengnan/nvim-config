ff16cf66bdaa6a9df23bc95e033fbcbbbcbc155a
Zhengnan Zhao <17503044+zzhengnan@users.noreply.github.com>
2026-04-28 23:53:46 -0400

    De-clutter lualina

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -26,32 +26,19 @@ return {
 		opts = {
 			options = {
 				theme = "vscode",
-				-- 	component_separators = { left = "", right = "" },
-				-- 	section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = {
-					{
-						"mode",
-						fmt = function(s)
-							return mode_map[s] or s
-						end,
-					},
-				},
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{
-						"filename",
-						-- 0: Just file name
-						-- 1: Relative path
-						-- 2: Absolute path
-						-- 3: Absolute path, with ~ as home directory
-						-- 4: File name and parent dir, with ~ as home directory
-						path = 4,
-						file_status = true, -- Displays file status (readonly status, modified status)
-					},
-				},
-				lualine_x = { nil },
+				-- 	lualine_a = {
+				-- 		{
+				-- 			"mode",
+				-- 			fmt = function(s)
+				-- 				return mode_map[s] or s
+				-- 			end,
+				-- 		},
+				-- 	},
+				lualine_b = { "branch" },
+				lualine_c = { "windows" },
+				lualine_x = { "diagnostics" },
 				lualine_y = { nil },
 				lualine_z = { "location" },
 			},
